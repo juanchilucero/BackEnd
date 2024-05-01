1465b5050744dc3c787bbf792c7102be8a0f0c69
Juan Ignacio Lucero Vazquez <juanchi98lucero@gmail.com>
2024-05-01 20:00:45 -0300

    corrigiendo app

--- src/app.js
+++ src/app.js
@@ -1,40 +1,15 @@
-import express from "express"
-import ProductManager from "./productManager.js";
-;
- // no reinvento la rueda, traigo el product manager del otro archivo
+import express from "express";
+import productsRoutes from "./routes/productsRoutes.js";
+import cartsRoutes from "./routes/cartsRoutes.js";
 
 const app = express();
-const port = 8080; //defino puerto
+const PORT = 8080;
 
-const manager = new ProductManager('./src/productos.json'); // esto me dio problemas, porque no especificaba la ruta me creaba 2 json
+app.use(express.json());
 
-app.get('/products', async (req, res) => {
-    try {
-        let products = manager.getProducts(); // cargamos los productos del json
+app.use("/api/products", productsRoutes);
+app.use("/api/carts", cartsRoutes);
 
-        //agregamos la opcion del limit
-        if (req.query.limit) {
-            const limit = parseInt(req.query.limit); // si por algun motivo no convierto el limit en numero entero no me va la cosa, asi que lo hago
-            products = products.slice(0, limit); // cortamos el array para que nos traiga la cantidad que queremos
-        }
-
-        res.json(products); //traemos lo solicitado
-    } catch (error) {
-        res.status(500).json({ error: error.message });//agarramos el error
-    }
-});
-
-//realizamos logica busqueda por id
-app.get('/products/:pid', async (req, res) => {
-    try {
-        const productId = parseInt(req.params.pid);// lo mismo de antes, pasamos el valor id como numero entero
-        const product = await manager.getProductById(productId);//uso la funcion del productManager para traer por id
-        res.json(product);
-    } catch (error) {
-        res.status(404).json({ error: error.message });//de nuevo agarramos error
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);// esto es para asegurarme que el puerto esta corriendo
+app.listen(PORT, () => {
+console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
